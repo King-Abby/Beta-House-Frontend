5e8735f192a5857ec40f5f23c650c30588f69c9d
king-Abby <iyiolaabby@gmail.com>
2024-12-09 12:11:19 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import FallBackLoader from "./components/FallBackLoader";
+import Homepage from "./pages/Homepage";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<FallBackLoader />}>
+                <Homepage />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="/signin"
+            element={
+              <Suspense fallback={<FallBackLoader />}>
+                <SignIn />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path="/signup"
+            element={
+              <Suspense fallback={<FallBackLoader />}>
+                <SignUp />
+              </Suspense>
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
