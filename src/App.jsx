b10c533cac654551458fe6f5e193a7d51bad68bf
king-Abby <iyiolaabby@gmail.com>
2024-12-09 12:59:00 +0100

    error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Suspense } from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import FallBackLoader from "./components/FallBackLoader";
